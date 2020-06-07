6d80b79fa8f217cf9de69ca67b717187bcb9ccf3
Farah MAKKI <farah.makki@adidas.com>
2020-06-07 18:23:12 +0200

    add serverless config, clean code

--- src/index.ts
+++ src/index.ts
@@ -38,24 +38,3 @@ const server = new ApolloServer({
 });
 
 exports.graphqlHandler = server.createHandler();
-
-// Construct a schema, using GraphQL schema language
-// const typeDefs = gql`
-//   type Query {
-//     hello: String
-//   }
-// `;
-
-// // Provide resolver functions for your schema fields
-// const resolvers = {
-//   Query: {
-//     hello: () => 'Hello world!'
-//   }
-// };
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers
-// });
-
-// exports.graphqlHandler = server.createHandler();
